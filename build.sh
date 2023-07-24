d31a327f007f55c22353b6ecb91b0865b1f91e72
Yang <yama5878@uni.sydney.edu.au>
2023-07-24 14:31:06 +1000

    add script for img convertion

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 export DOCKER_TMP=/data/users/yang/HPC/docker/tmp
-sudo docker build . -t bitmayang/monai:1.2.0
+sudo docker build . -t bitmayang/monai
+#:1.2.0
+sudo docker push bitmayang/monai
---
+++ convert_singularity.sh
@@ -0,0 +1,4 @@
+export SINGULARITY_CACHEDIR=`pwd`
+export SINGULARITY_TMPDIR=`pwd`
+
+sudo singularity build monai.img docker://bitmayang/monai
