64455d0ef8ecd888b8694ed6e85cd52711b540d1
Yang <yama5878@uni.sydney.edu.au>
2023-07-26 11:38:31 +1000

    add monai dependency

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 export DOCKER_TMP=/data/users/yang/HPC/docker/tmp
-sudo docker build . -t bitmayang/monai
-#:1.2.0
-sudo docker push bitmayang/monai
+sudo docker build . -t bitmayang/ubuntu1604:monai.1.0
+sudo docker push bitmayang/ubuntu1604:monai.1.0
