7607df170f897d905649657c99c3da29970efac2
Yang <yama5878@uni.sydney.edu.au>
2023-07-27 10:40:07 +1000

    add monai dependency

--- convert_singularity.sh
+++ convert_singularity.sh
@@ -1,4 +1,4 @@
 export SINGULARITY_CACHEDIR=`pwd`
 export SINGULARITY_TMPDIR=`pwd`
 
-sudo -E singularity build monai.img docker://bitmayang/monai
+sudo -E singularity build monai.img docker://bitmayang/ubuntu1604:monai.1.0
